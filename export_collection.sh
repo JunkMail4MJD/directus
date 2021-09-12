0563c18b65b2cd3a50be372b5d6de7056b5c6bad
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-12 14:07:37 -0400

    Update export_collection.sh
    parameterized the URL

--- export_collection.sh
+++ export_collection.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 COLLECTION="${1}"
 TOKEN="${DIRECTUS_TOKEN}"
-curl --location --request GET "http://localhost:8055/collections/${COLLECTION}" \
+PROTOCOL="http"
+HOST="localhost:8055"
+
+curl --location --request GET "${PROTOCOL}://${HOST}/collections/${COLLECTION}" \
 --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data' > ${COLLECTION}_collection.json
 
-curl --location --request GET "http://localhost:8055/fields/${COLLECTION}" \
+curl --location --request GET "${PROTOCOL}://${HOST}/fields/${COLLECTION}" \
 --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' | \
 jq -c 'if .schema.is_primary_key == true then del( .schema.is_unique ) else .  end' > ${COLLECTION}_fields.json
 
-curl --location --request GET "http://localhost:8055/relations/${COLLECTION}" \
+curl --location --request GET "${PROTOCOL}://${HOST}/relations/${COLLECTION}" \
 --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' > ${COLLECTION}_relations.json
 
-curl --location --request GET "http://localhost:8055/items/${COLLECTION}" \
+curl --location --request GET "${PROTOCOL}://${HOST}/items/${COLLECTION}" \
 --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' > ${COLLECTION}_data.json
 
 cat ${COLLECTION}_collection.json | jq -c --slurpfile fields ${COLLECTION}_fields.json '. + { fields: $fields }' > ${COLLECTION}_schema.json
