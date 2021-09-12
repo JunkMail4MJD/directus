971ab292d77b8bd500b1d9a154e8d20d5a92d033
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-12 14:15:47 -0400

    Create restore_collections_backup.sh

---
+++ restore_collections_backup.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+set -e
+
+printf "\n\n*************************************\n"; 
+printf "\n\nGetting list of user collections:\n"; 
+
+TOKEN="${DIRECTUS_TOKEN}"
+PROTOCOL="http"
+HOST="localhost:8055"
+
+# POST "/collections"
+printf "\n\n*************************************\n"; 
+printf "Restoring user collections to: ${PROTOCOL}://${HOST}/\n"
+while read -r line; do
+    COLLECTION="$line"
+    printf "Read line from config file: ${COLLECTION}\n"
+
+    FILENAME="backup/${COLLECTION}_schema.json"
+    URL="${PROTOCOL}://${HOST}/collections"
+    while read -r line; do
+        record="$line"
+        printf "\n\nSchema read from file: ${FILENAME}\n\n"
+
+        curl --location --request POST "${URL}" \
+            --header "Authorization: Bearer ${TOKEN}" \
+            --header 'Content-Type: application/json' \
+            --data-raw "${record}"
+
+        if [ $? -eq 0 ]; then
+            printf "\n\n...created user collection: ${COLLECTION} at: ${URL}\n"
+        else
+            printf "\n\nFAILED to create user collection: ${COLLECTION} at: ${URL}\n $?\n\n";
+            exit 1
+        fi
+    done < "$FILENAME"
+done < backup/collection_list.txt
+printf "\n\nFinished creating collection schemas!\n\n"; 
+
+# POST "/relations"
+printf "\n\n*************************************\n"; 
+printf "Restoring collection relationships to: ${PROTOCOL}://${HOST}/\n"
+while read -r line; do
+    COLLECTION="$line"
+    printf "Read line from config file: ${COLLECTION}\n"
+
+    FILENAME="backup/${COLLECTION}_relations.json"
+    URL="${PROTOCOL}://${HOST}/relations"
+    while read -r line; do
+        record="$line"
+        printf "\n\nRead relationship from file: ${FILENAME}\n\n"
+
+        curl --location --request POST "${URL}" \
+            --header "Authorization: Bearer ${TOKEN}" \
+            --header 'Content-Type: application/json' \
+            --data-raw "${record}"
+
+        if [ $? -eq 0 ]; then
+            printf "\n\n...created collection relationships for ${COLLECTION} at: ${URL}\n"
+        else
+            printf "\n\nFAILED to create collection relationships for ${COLLECTION} at: ${URL}\n $?\n\n";
+            exit 1
+        fi
+    done < "$FILENAME"
+done < backup/collection_list.txt
+printf "\n\nFinished creating collection relationships!\n\n"; 
+
+# POST "/items/":collection
+printf "\n\n*************************************\n"; 
+printf "Restoring collection data to: ${PROTOCOL}://${HOST}/\n"
+while read -r line; do
+    COLLECTION="$line"
+    printf "Read line from config file: ${COLLECTION}\n"
+
+    FILENAME="backup/${COLLECTION}_data.json"
+    URL="${PROTOCOL}://${HOST}/items/${COLLECTION}"
+    while read -r line; do
+        record="$line"
+        printf "\n\nRead record from file: ${FILENAME}\n\n"
+
+        curl --location --request POST "${URL}" \
+            --header "Authorization: Bearer ${TOKEN}" \
+            --header 'Content-Type: application/json' \
+            --data-raw "${record}"
+
+        if [ $? -eq 0 ]; then
+            printf "\n\n...Added record to collection: ${COLLECTION} at: ${URL}\n"
+        else
+            printf "\n\nFAILED to add record to collection: ${COLLECTION} at: ${URL}\n $?\n\n";
+            exit 1
+        fi
+    done < "$FILENAME"
+done < backup/collection_list.txt
+printf "\n\nFinished restoring data!\n\n"; 
+printf "\n\n*************************************\n"; 
+printf "\n\nFinished !!!!!\n\n"; 
+printf "\n\n*************************************\n"; 
