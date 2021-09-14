bff8231d1e14a8da551018addb8cfd78c49b7135
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-14 12:19:33 -0400

    Update load_relationships.sh

--- load_relationships.sh
+++ load_relationships.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
-PROTOCOL="http"
-HOST="localhost:8055"
-TOKEN="${DIRECTUS_TOKEN}"
-COLLECTIONS=("country" "state" "city" "pages")
+set -e
+HOST="${1}"
+TOKEN="${2}"
+COLLECTION="${3}"
+
+URL="${HOST}/collections"
 
 # POST "/relations"
 printf "\n\n*************************************\n"; 
-printf "\n\nCreating relations:\n"; 
-for COLLECTION in "${COLLECTIONS[@]}"; do 
-    printf "\n\n...defining relations for: ${COLLECTION}\n"; 
-    FILENAME="${COLLECTION}_relations.json"
-    PATH_ELEMENT="/relations"
-    URL="${PROTOCOL}://${HOST}${PATH_ELEMENT}"
-    while read -r line; do
-        record="$line"
-        printf "\n\nRecord read from file: ${FILENAME}\n\n"
-        curl --location --request POST "${URL}" \
-            --header "Authorization: Bearer ${TOKEN}" \
-            --header 'Content-Type: application/json' \
-            --data-raw "${record}"
-    done < "$FILENAME"
-done
+printf "\n\nCreating relationships for: ${COLLECTION}\n"; 
+FILENAME="temp/${COLLECTION}_relations.json"
+URL="${HOST}/relations"
+while read -r line; do
+    record="$line"
+    printf "\n\nRead relationship from file: ${FILENAME}\n\n"
+    curl --location --request POST "${URL}" \
+        --header "Authorization: Bearer ${TOKEN}" \
+        --header 'Content-Type: application/json' \
+        --data-raw "${record}"
+    if [ $? -eq 0 ]; then
+        printf "\n\n...created relationship for ${COLLECTION} at: ${URL}\n"
+    else
+        printf "\n\nFAILED to create relationship for ${COLLECTION} at: ${URL}\n $?\n\n";
+        exit 1
+    fi
+done < "$FILENAME"
+
 printf "\n\n...finished defining relations.\n"; 
