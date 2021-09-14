24b023d2f632278f5e83175c6e49b758443178d8
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-14 12:20:55 -0400

    Update load_data.sh

--- load_data.sh
+++ load_data.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-PROTOCOL="http"
-HOST="localhost:8055"
-TOKEN="${DIRECTUS_TOKEN}"
-COLLECTION="${1}"
+set -e
+HOST="${1}"
+TOKEN="${2}"
+COLLECTION="${3}"
 
 # POST "/items/":collection
 printf "\n\n*************************************\n"; 
-printf "\n\nCreating items:\n"; 
-printf "\n\n...creating items for: ${COLLECTION}\n"; 
-FILENAME="${COLLECTION}_data.json"
-PATH_ELEMENT="/items/${COLLECTION}"
-URL="${PROTOCOL}://${HOST}${PATH_ELEMENT}"
+printf "\n\nCreating items for: ${COLLECTION}\n"; 
+FILENAME="temp/${COLLECTION}_data.json"
+URL="${HOST}/items/${COLLECTION}"
 while read -r line; do
     record="$line"
-    printf "\n\nRecord read from file: ${FILENAME}\n\n"
+    printf "\n\nRead item from file: ${FILENAME}\n\n"
     curl --location --request POST "${URL}" \
         --header "Authorization: Bearer ${TOKEN}" \
         --header 'Content-Type: application/json' \
         --data-raw "${record}"
+        if [ $? -eq 0 ]; then
+            printf "\n\n...Added record to collection: ${COLLECTION} at: ${URL}\n"
+        else
+            printf "\n\nFAILED to add record to collection: ${COLLECTION} at: ${URL}\n $?\n\n";
+            exit 1
+        fi
 done < "$FILENAME"
 printf "\n\n...finished loading data.\n"; 
