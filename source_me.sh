a0f4a101125a49166d143a1cd5c0e68a9b88a5a4
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-14 12:25:24 -0400

    Create source_me.sh

---
+++ source_me.sh
@@ -0,0 +1,7 @@
+export DIRECTUS_QA_HOST="https://your.qa.site.goes.here.com"
+export DIRECTUS_DEV_HOST="https://your.dev.site.goes.here.com"
+export DIRECTUS_LOCALHOST="http://localhost:8055"
+
+export DIRECTUS_QA_TOKEN="Your-Token-For-QA-environment"
+export DIRECTUS_DEV_TOKEN="Your-Token-For-DEV-environment"
+export DIRECTUS_LOCALHOST_TOKEN="Your-Token-For-localhost"
