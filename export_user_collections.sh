a1201a3afafa83522ace263eae8828ca1d0bbb7d
JunkMail4MJD <JunkMail4MJD@users.noreply.github.com>
2021-09-13 21:17:01 -0400

    Update export_user_collections.sh
    * strip out id fields that turn into primary keys in the database
    * strip out created by and updated by user IDs. when moving between environments. The GUIDs will be different.

--- export_user_collections.sh
+++ export_user_collections.sh
@@ -34,7 +34,7 @@ if [ $? -eq 0 ]; then
         URL="${PROTOCOL}://${HOST}/fields/${COLLECTION}"
         curl --location --request GET "${URL}" \
             --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' | \
-            jq -c 'if .schema.is_primary_key == true then del( .schema.is_unique ) else .  end' > backup/${COLLECTION}_fields.json
+            jq -c 'if .schema.is_primary_key == true then del( .schema.is_unique ) else .  end' | jq -c 'del( .meta.id) | .' > backup/${COLLECTION}_fields.json
         if [ $? -eq 0 ]; then
             printf "...retrieved the collection's field definitions from: ${URL}\n"
         else
@@ -44,7 +44,7 @@ if [ $? -eq 0 ]; then
 
         URL="${PROTOCOL}://${HOST}/relations/${COLLECTION}"
         curl --location --request GET "${URL}" \
-            --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' > backup/${COLLECTION}_relations.json
+            --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' | jq -c 'del( .meta.id) | .' > backup/${COLLECTION}_relations.json
         if [ $? -eq 0 ]; then
             printf "...retrieved the collection's relationship definitions from: ${URL}\n"
         else
@@ -54,7 +54,7 @@ if [ $? -eq 0 ]; then
 
         URL="${PROTOCOL}://${HOST}/items/${COLLECTION}"
         curl --location --request GET "${URL}" \
-            --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' > backup/${COLLECTION}_data.json
+            --header "Authorization: Bearer ${TOKEN}" | jq -cs '.[] | .data | .[]' | jq -c 'del( .user_created) | del( .user_updated) | del( .created_by) | del( .updated_by ) | .' > backup/${COLLECTION}_data.json
         if [ $? -eq 0 ]; then
             printf "...retrieved the collection's data from: ${URL}\n"
         else
@@ -71,6 +71,9 @@ if [ $? -eq 0 ]; then
             exit 1
         fi
     done < backup/collection_list.txt
+
+    URL="${PROTOCOL}://${HOST}/utils/cache/clear"
+    curl --location --request POST "${URL}" --header "Authorization: Bearer ${TOKEN}"
     printf "\n\nFinished!\n\n"; 
 else
     printf "\n\nFAILED to get the list of user collections:$?\n\n"; 
